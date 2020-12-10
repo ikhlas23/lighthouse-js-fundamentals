094ff5c31daa4216e47a8bc484a1a9afb0dccd73
Eklas Abdi <eklasabdi@gmail.com>
2020-12-10 14:40:05 +0000

    loopy lighthouse to function to hello.js

---
+++ loopy-lighthouse.js
@@ -0,0 +1,14 @@
+for (let loopy = 10; loopy <=20; loopy++) {
+  if (loopy % 3 === 0 && loopy % 4 === 0) {
+    console.log("LoopyLighthouse")
+  } else if (loopy % 3 === 0) {
+    console.log("Loopy"); 
+  } else if (loopy % 4 === 0) {
+    console.log("Lighthouse"); 
+  } else {
+    console.log(loopy);
+}; 
+}
+
+
+
--- whichSchool.js
+++ whichSchool.js
@@ -1,20 +1,20 @@
 /**/ 
 
-
 const whichSchool  = function (age) {
-  if (age < 13 ) {
+  if (age < 13){
     return "Elementary School"; 
-// if the student is under 13; display Elementary school
-  } else if (age >= 13 && age <= 18) {
+  } else if (age >=13 && age <=18) {
     return "Secondary School"; 
-// if the student is between 13 and 18; display Secondary school
-  } else {
+  } else if(age > 18 || age <=100) {
     return "Lighthouse Labs"; 
-// if the student is above 18; return Lighthouse Labs
   }
-}
+  }
+  
+  console.log(whichSchool(23)); 
 
-console.log(whichSchool(12)); 
-console.log(whichSchool(35)); 
-console.log(whichSchool(17)); 
+// console.log(whichSchool(12)); 
+// console.log(whichSchool(35)); 
+// console.log(whichSchool(17)); 
+
+
 
